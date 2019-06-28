c6a4e7e5a372d0828844ab56d3f0b13d6922a7d6
Kriyu <thcavailles@laposte.net>
2019-06-28 15:41:16 +0200

    Create ApiControllerTest, create tests for tests getUserAction (ApiController), test access if user is not admin, test url with null request header, test with bad request method, test with bad url param

---
+++ tests/AppBundle/Controller/ApiControllerTest.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * ApiController Test
+ */
+
+namespace Tests\AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+/**
+ * ApiControllerTest
+ */
+class ApiControllerTest extends WebTestCase
+{
+    /**
+     * @var Client
+     * @access private
+     */
+    private $client;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp(): void
+    {
+        $this->client = self::createClient();
+    }
+
+    /**
+     * Test getUsersAction method of ApiController (Ajax)
+     * @access public
+     *
+     * @return void
+     */
+    public function testGetUsers(): void
+    {
+        $this->logIn('admin');
+
+        $this->client->request(
+            'GET',
+            '/api/users/1',
+            array(),
+            array(),
+            array('HTTP_X-Requested-With' => 'XMLHttpRequest')
+        );
+
+        $datas = json_decode($this->client->getResponse()->getContent(), true);
+        
+        $firstUser = $datas[0];
+
+        $this->assertEquals(10, count($datas));
+
+        $this->assertNotEmpty($firstUser['id']);
+        $this->assertNotEmpty($firstUser['username']);
+        $this->assertNotEmpty($firstUser['email']);
+        $this->assertNotEmpty($firstUser['role']);
+    }
+
+    /**
+     * Test user can't access if not admin
+     * @access public
+     *
+     * @return void
+     */
+    public function testUserCantAccessIfNotAdmin(): void
+    {
+        $this->logIn('secondary');
+
+        $this->client->request(
+            'GET',
+            '/api/users/1',
+            array(),
+            array(),
+            array('HTTP_X-Requested-With' => 'XMLHttpRequest')
+        );
+
+        $statusCode = $this->client->getResponse()->getStatusCode();
+
+        $this->assertEquals(403, $statusCode);
+    }
+
+    /**
+     * Test user can't access if not auth
+     * @access public
+     * @dataProvider apiUrlProvider
+     *
+     * @return void
+     */
+    public function testUserCantAccessIfNotAuth($url): void
+    {
+        $this->client->request(
+            'GET',
+            $url,
+            array(),
+            array(),
+            array('HTTP_X-Requested-With' => 'XMLHttpRequest')
+        );
+
+        $crawler = $this->client->followRedirect();
+        $this->assertSame(' To Do List - Login', $crawler->filter('title')->text());
+    }
+
+    /**
+     * Api url provider
+     * @access public
+     * 
+     * @return array
+     */
+    public function apiUrlProvider(): array
+    {
+        return [
+            [
+                '/api/users/1',
+            ]
+        ];
+    }
+
+    /**
+     * Test url with null request header (Ajax)
+     * @access public
+     * @dataProvider apiUrlProvider
+     *
+     * @return void
+     */
+    public function testUrlWithNullRequestHeader($url): void
+    {
+        $this->logIn('admin');
+
+        $this->client->request('GET', $url);
+
+        $this->assertTrue($this->client->getResponse()->isNotFound());
+    }
+
+    /**
+     * Test url with a bad request method (Ajax)
+     * @access public
+     * @dataProvider apiUrlProvider
+     *
+     * @return void
+     */
+    public function testUrlWithBadRequestMethod($url): void
+    {
+        $this->logIn('admin');
+
+        $this->client->request(
+            'PATCH',
+            $url,
+            array(),
+            array(),
+            array('HTTP_X-Requested-With' => 'XMLHttpRequest')
+        );
+
+        $this->assertEquals(405, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test getComments with a bad request method (Ajax)
+     * @access public
+     * @dataProvider apiUrlBadParamProvider
+     *
+     * @return void
+     */
+    public function testUrlWithBadUrlParam($url): void
+    {
+        $this->logIn('admin');
+
+        $this->client->request(
+            'GET',
+            $url,
+            array(),
+            array(),
+            array('HTTP_X-Requested-With' => 'XMLHttpRequest')
+        );
+
+        $this->assertEquals(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Api url with bad param
+     * @access public
+     *
+     * @return array
+     */
+    public function apiUrlBadParamProvider(): array
+    {
+        return [
+            [
+                '/api/users/test'
+            ]
+        ];
+    }
+
+    /**
+     * Log user
+     * @access private
+     * @param string $userRole
+     *
+     * @return void
+     */
+    private function logIn($userRole): void
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewallName = 'main';
+        $firewallContext = 'main';
+        $manager = $this->client->getContainer()->get('doctrine')->getManager();
+
+        if ($userRole == 'admin') {
+            $user = $manager->getRepository(User::class)->findOneByUsername('SuperAdmin'); 
+        }
+
+        if ($userRole == 'secondary') {
+            $user = $manager->getRepository(User::class)->findOneByUsername('JeanTest'); 
+        }
+
+        $token = new UsernamePasswordToken($user, $user->getPassword(), $firewallName, $user->getRoles());
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown(): void
+    {
+        $this->client = null;
+    }
+}
